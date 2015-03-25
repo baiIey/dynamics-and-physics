551f340cfb91f18520045d7da4f9b7db615afd08
Brian Bailey <brian@bailey.is>
2015-03-24 18:09:23 -0700

    Initialize Xcode

---
+++ Snow Fall/Snow FallTests/Snow_FallTests.swift
@@ -0,0 +1,36 @@
+//
+//  Snow_FallTests.swift
+//  Snow FallTests
+//
+//  Created by Brian Bailey on 3/24/15.
+//  Copyright (c) 2015 Brian Bailey. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Snow_FallTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
